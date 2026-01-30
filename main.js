a6c5069a43cca212993b68101de3ef5a61aca75a
blackrosebloom <226smuller@frhsd.com>
2026-01-30 09:58:13 -0500

    event handling

--- main.js
+++ main.js
@@ -1,17 +1,26 @@
 const app = Vue.createApp({
 data() {
 return {
+    cart: 0,
     product: 'socks',
     image:'socks_green.jpg',
     instock:true,
     details: ['50% cotton', '30% wool', '20% polyester'],
     variants: [
-    {id: 2234, color: 'green'},
-    {id: 2235, color: 'blue'},
+    {id: 2234, color: 'green', image: 'socks_green.jpg'},
+    {id: 2235, color: 'blue', image: 'socks_blue.jpg'},
     ]
 }
 
 
+},
+methods: {
+    addtocart() {
+        this.cart += 1
+    },
+    updateImage(variantImage) {
+        this.image = variantImage
+    }
 }
 
 })
